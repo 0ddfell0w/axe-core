d403b90687be1e268b607c9070004085a67d0d61
Zack Goldberg <zsgoldberg@gmail.com>
2017-12-11 14:51:53 -0500

    rename to focus-order-invalid-role

--- test/checks/keyboard/focus-order-invalid-role.js
+++ test/checks/keyboard/focus-order-invalid-role.js
@@ -1,9 +1,8 @@
-describe('focusable-invalid-role', function () {
+describe('focus-order-invalid-role', function () {
 	'use strict';
 
 	var fixture = document.getElementById('fixture');
 	var fixtureSetup = axe.testUtils.fixtureSetup;
-	// var shadowSupport = axe.testUtils.shadowSupport;
 
 	afterEach(function () {
 		fixture.innerHTML = '';
@@ -12,13 +11,13 @@ describe('focusable-invalid-role', function () {
 	it('should pass if explicit tabindex < 0', function () {
 		fixtureSetup('<span tabindex="-1"></span>');
 		var node = fixture.querySelector('span');
-		assert.isFalse(checks['focusable-invalid-role'].evaluate(node));
+		assert.isFalse(checks['focus-order-invalid-role'].evaluate(node));
 	});
 
 	it('should pass if it has a widget role', function () {
-		fixtureSetup('<span role="button">Button</span>');
+		fixtureSetup('<span tabindex="0" role="button">Button</span>');
 		var node = fixture.querySelector('span');
-		assert.isFalse(checks['focusable-invalid-role'].evaluate(node));
+		assert.isFalse(checks['focus-order-invalid-role'].evaluate(node));
 	});
 
 });
